24223c38f28162c2f46c52a43d2cf572d6603359
Anatoly Scherbakov <altaisoft@gmail.com>
2021-06-28 10:36:57 +0700

    CBS-3545 Correct field names

--- sqs_async/main.tf
+++ sqs_async/main.tf
@@ -1,9 +1,9 @@
 locals {
   maybe_retries = var.retry_policies == null ? {} : {
-    retry: [for module in var.retry_policies: module.retry]
+    Retry: [for module in var.retry_policies: module.retry]
   }
 
   maybe_catches = var.catch_policies == null ? {} : {
-    catch: [for module in var.catch_policies: module.catch]
+    Catch: [for module in var.catch_policies: module.catch]
   }
 }
