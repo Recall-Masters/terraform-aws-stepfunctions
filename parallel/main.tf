34c2537632aa70a90f56b5f413495f67915b5d04
Anton Novosyolov <anton.novosyolov@gmail.com>
2021-08-23 11:52:50 +0300

    CB-64 Add additional_states argument for parallel

--- parallel/main.tf
+++ parallel/main.tf
@@ -1,11 +1,14 @@
 locals {
   branches = [for mod in var.steps: {
     StartAt = mod.step != null ? mod.name : mod.start_at
-    States = jsondecode(mod.step != null ? jsonencode({
-      (mod.name): merge(
-        mod.step,
-        { End: true }
-      )
-    }) : jsonencode(mod.states))
+    States = merge(
+      jsondecode(mod.step != null ? jsonencode({
+        (mod.name): merge(
+          mod.step,
+          { End: true }
+        )
+      }) : jsonencode(mod.states)),
+      var.additional_states
+    )
   }]
 }
