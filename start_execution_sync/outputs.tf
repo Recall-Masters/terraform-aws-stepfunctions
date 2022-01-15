3f32b0fa3bac7bb4313db0b0a7286a1fd98a5743
Anton Novosyolov <anton.novosyolov@gmail.com>
2022-01-15 21:50:50 +0300

    feat: support step_object for start_execution_sync

--- start_execution_sync/outputs.tf
+++ start_execution_sync/outputs.tf
@@ -2,21 +2,14 @@ output name {
   value = var.name
 }
 
-output step {
-  value = {
-    Type: "Task"
-    Resource: "arn:aws:states:::states:startExecution.sync:2"
-    Parameters: {
-      StateMachineArn: var.state_machine_arn
-      Input: merge(
-        var.input,
-        {
-          Comment: var.comment
-          "AWS_STEP_FUNCTIONS_STARTED_BY_EXECUTION_ID.$": "$$.Execution.Id"
-        }
-      )
-    }
 
-    ResultPath: var.result_path
+output step {
+  value = local.step
+}
+
+
+output step_object {
+  value = {
+    (var.name) : local.step
   }
 }
