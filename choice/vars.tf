b790d2dffc44e3428e6c02b0be0d2c38672afb30
Anton Novosyolov <anton.novosyolov@gmail.com>
2022-01-15 21:33:17 +0300

    feat: add choice module

---
+++ choice/vars.tf
@@ -0,0 +1,16 @@
+variable default {
+  description = "The name of step for the default choice."
+  type        = string
+}
+
+
+variable choices {
+  description = "Choices list."
+}
+
+
+variable name {
+  description = "Name of the step."
+  type        = string
+  default     = ""
+}
